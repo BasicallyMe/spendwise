a8322377870d6d08feaadcd677914e251779ea72
Abajit Dey <abajitdeyag10118@gmail.com>
2023-07-15 10:36:47 +0530

    Removed all files for fresh start

--- client/src/components/Icons/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Logo } from "./Logo";
-export { default as Icon } from "./Icon";
-export { default as Mastercard } from "./MasterCard";
-export { default as Visa } from "./Visa";
--- client/src/components/SignUp/Register.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Register = () => {
-    return <h1>Just a few more steps</h1>
-}
-
-export default Register;
