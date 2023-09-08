d2139c8d7bef5992bc985b2c8c8b1e70b1401f98
Abajit Dey <abajitdeyag10118@gmail.com>
2023-09-08 10:49:24 +0530

    Updated layout, added functionality for expense summary
    Updated layout for the transaction page, added functionality to calculate summary of expenses and added logic to show and hide loading state

--- src/app/user/transactions/page.tsx
+++ src/app/user/transactions/page.tsx
@@ -11,7 +11,13 @@ import LoadingSkeleton from "./loadingSkeleton";
 
 export default function Transactions() {
   const { transactions } = useTransactionContext();
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [expenseSummary, setExpenseSummary] = useState({
+    income: null,
+    investments: null,
+    essentials: null,
+    personal: null,
+  });
   const [currentMonth, setCurrentMonth] = useStore((state) => [
     state.currentMonth,
     state.setCurrentMonth,
@@ -20,11 +26,36 @@ export default function Transactions() {
 
   // Update filteredTransactions whenever currentMonth changes
   useEffect(() => {
+    setLoading(true);
     const filtered = transactions.filter((transaction) => {
       return transaction.month === currentMonth; // Assuming 'month' is a property in your transaction data
     });
     setFilteredTransactions(filtered);
   }, [transactions, currentMonth]);
+
+  useEffect(() => {
+    let summary = { income: 0, investments: 0, essentials: 0, personal: 0 };
+    if (filteredTransactions.length !== 0) {
+      filteredTransactions.map((item) => {
+        switch (item.type) {
+          case "income":
+            summary.income = summary.income + parseInt(item.amount);
+            break;
+          case "investments":
+            summary.investments = summary.investments + parseInt(item.amount);
+            break;
+          case "essentials":
+            summary.essentials = summary.essentials + parseInt(item.amount);
+            break;
+          case "personal":
+            summary.personal = summary.personal + parseInt(item.amount);
+            break;
+        }
+      });
+    }
+    setExpenseSummary({ ...summary });
+    setLoading(false);
+  }, [filteredTransactions]);
 
   const handleMonthChange = (event) => {
     const selectedMonthIndex = parseInt(event.target.value, 10);
@@ -37,8 +68,9 @@ export default function Transactions() {
 
   return (
     <div className="w-full h-full relative">
-      <div className="flex flex-row justify-end px-3 py-3 my-4">
-        <div className="flex flex-wrap gap-1">
+      <div className="my-5">
+        <h2 className="text-sm font-normal text-slate-500 mb-2">Your summary</h2>
+        <div className="flex flex-row flex-wrap gap-1">
           <span className="mr-3 text-sm p-2 rounded-md text-white bg-blue-400">
             Month:{" "}
             <span className="text-slate-800">
@@ -46,29 +78,38 @@ export default function Transactions() {
             </span>
           </span>
           <span className="mr-3 text-sm text-white bg-green-400 p-2 rounded-md">
-            Income: <span className="text-slate-800">1000</span>
+            Income:{" "}
+            <span className="text-slate-800">{expenseSummary.income}</span>
           </span>
           <span className="mr-3 text-sm text-white bg-amber-400 p-2 rounded-md">
-            Essentials: <span className="text-slate-800">1000</span>
+            Essentials:{" "}
+            <span className="text-slate-800">{expenseSummary.essentials}</span>
           </span>
           <span className="mr-3 text-sm text-white bg-purple-400 p-2 rounded-md">
-            Investments: <span className="text-slate-800">1000</span>
+            Investments:{" "}
+            <span className="text-slate-800">{expenseSummary.investments}</span>
           </span>
-          <span className="mr-3 text-sm text-white bg-orange-400 p-2 rounded-md">
-            Expenses: <span className="text-slate-800">1000</span>
+          <span className="text-sm text-white bg-orange-400 p-2 rounded-md">
+            Personal:{" "}
+            <span className="text-slate-800">{expenseSummary.personal}</span>
           </span>
-          <select
-            className="text-sm p-2 rounded-md border"
-            value={currentMonth}
-            onChange={handleMonthChange}
-          >
-            {months.map((month, index) => (
-              <option key={index.toString()} value={index} className="py-2">
-                {month}
-              </option>
-            ))}
-          </select>
         </div>
+      </div>
+      <div className="pl-2 pr-3 flex flex-row items-center justify-between">
+        <h2 className="text-sm font-normal text-slate-500">
+          Your transactions for {getMonthString(currentMonth)}
+        </h2>
+        <select
+          className="text-sm p-2 rounded-md border"
+          value={currentMonth}
+          onChange={handleMonthChange}
+        >
+          {months.map((month, index) => (
+            <option key={index.toString()} value={index} className="py-2">
+              {month}
+            </option>
+          ))}
+        </select>
       </div>
       <table className="border-separate border-spacing-2 w-full">
         <thead>
@@ -127,7 +168,9 @@ export default function Transactions() {
       </table>
       {!loading && filteredTransactions.length === 0 && (
         <div className="text-center text-sm text-slate-400 py-5">
-          <p>We couldn't find any transactions for this month. Try adding a few.</p>
+          <p>
+            We couldn't find any transactions for this month. Try adding a few.
+          </p>
         </div>
       )}
     </div>
